4dfbcf8c864be3cc5239fe3214a14b29b5f9baa6
rgiinther <r.giin95@gmail.com>
2022-08-07 00:48:31 -0500

    end of 22.4

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,9 +5,14 @@ import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
 import { idbPromise } from "../../utils/helpers";
+import { useLazyQuery } from '@apollo/client';
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -19,6 +24,14 @@ const Cart = () => {
           getCart();
         }
       }, [state.cart.length, dispatch]);
+
+      useEffect(() => {
+        if (data) {
+          stripePromise.then((res) => {
+            res.redirectToCheckout({ sessionId: data.checkout.session });
+          });
+        }
+      }, [data]);
       
 function toggleCart() {
   dispatch({ type: TOGGLE_CART });
@@ -29,6 +42,19 @@ function calculateTotal() {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+
+    getCheckout({
+      variables: { products: productIds }
+    });
   }
 if (!state.cartOpen) {
     return (
@@ -53,7 +79,7 @@ if (!state.cartOpen) {
         <strong>Total: ${calculateTotal()}</strong>
         {
           Auth.loggedIn() ?
-            <button>
+            <button onClick={submitCheckout}>
               Checkout
             </button>
             :
